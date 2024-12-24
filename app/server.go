76480f52fd956def160988608f06d118655f1576
Joey Yu <joey@jyu.dev>
2024-12-24 18:26:21 -0500

    feat: parse correlation id

--- app/server.go
+++ app/server.go
@@ -19,5 +19,12 @@ func main() {
 	}
 	defer conn.Close()
 
-	conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
+	data := make([]byte, 1024)
+	conn.Read(data)
+
+	resp := make([]byte, 8)
+	copy(resp, []byte{0, 0, 0, 0})
+	copy(resp, data[8:13])
+
+	conn.Write(resp)
 }
