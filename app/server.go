e84df13b28601f3bdb3f5783e3703ebaa47685bc
Joey Yu <joey@jyu.dev>
2024-12-24 17:34:11 -0500

    feat: send correlation id

--- app/server.go
+++ app/server.go
@@ -12,9 +12,12 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7})
 }
